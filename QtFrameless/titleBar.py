6fd4d38ee647993d1e7f635e65e426e85b0ef0ca
Alex <alexpdev@protonmail.com>
2023-01-08 10:35:56 -0800

    removed edge cursors

--- QtFrameless/titleBar.py
+++ QtFrameless/titleBar.py
@@ -1,10 +1,8 @@
 import os
 from QtFrameless.qt_api import QLabel, QWidget, QSizePolicy, QHBoxLayout, QPixmap
 from QtFrameless.button import TitleBarButton
-from QtFrameless.cursor import Cursor
 
 icon = os.path.join(os.path.dirname(__file__), "home.png")
-
 
 class TitleBar(QWidget):
     def __init__(self, parent=None):
@@ -33,8 +31,6 @@ class TitleBar(QWidget):
         self.setMouseTracking(True)
         self._pressed = False
         self._cpos = None
-        self._cgeom = None
-        self._direction = None
 
     def setWindowTitle(self, title: str) -> None:
         """Sets the title bar label."""
@@ -45,40 +41,26 @@ class TitleBar(QWidget):
         pixmap = QPixmap(icon)
         self.icon.setPixmap(pixmap)
 
-    def mousePressEvent(self, event):
-        pos = event.position().toPoint()
-        rect = self.window().rect()
-        direction = Cursor.match(pos, rect, exclusions=[5, 3, 2])
-        self._direction = direction
-        self._cgeom = self.window().geometry()
-        self._cpos = pos
-        self._pressed = True
+    def mouseDoubleClickEvent(self, _):
+        if self.window().isMaximized():
+            self.window().showNormal()
+        else:
+            self.window().showMaximized()
 
-    def mouseReleaseEvent(self, _):
-        self._pressed = False
-        self._cpos = None
-        self._direction = None
-        self._cgeom = None
+    def mousePressEvent(self, event):
+        self._pressed = True
+        self._cpos = event.position().toPoint()
 
     def mouseMoveEvent(self, event):
-        window = self.window()
-        geom = window.geometry()
+        if not self._pressed:
+            return super().mouseMoveEvent(event)
         pos = event.position().toPoint()
-        if self._pressed:
-            r = Cursor.resize(self._cpos, pos, geom, self._cgeom, self._direction)
-            min_width, min_height = window.minimumSizeHint().toTuple()
-            if r[2] < min_width or r[3] < min_height:
-                return
-            return window.setGeometry(*r)
-        direction = Cursor.match(pos, geom, [5, 3, 2])
-        self.setCursor(direction["shape"])
-
-    def mouseDoubleClickEvent(self, event):
-        pos = event.position().toPoint()
-        std = Cursor.loc[8]
+        difx, dify = (pos - self._cpos).toTuple()
         geom = self.window().geometry()
-        if std["range"](pos, geom):
-            if self.window().isMaximized():
-                self.window().showNormal()
-            else:
-                self.window().showMaximized()
+        x, y, w, h = geom.x(), geom.y(), geom.width(), geom.height()
+        new_coords = x+difx, y+dify, w, h
+        self.window().setGeometry(*new_coords)
+
+    def mouseReleaseEvent(self, event):
+        self._pressed = False
+        self._cpos = None
--- QtFrameless/window.py
+++ QtFrameless/window.py
@@ -4,13 +4,10 @@ from QtFrameless.qt_api import (
     QMainWindow,
     QVBoxLayout,
     QWidget,
-    Qt,
-    QEvent,
-    QMouseEvent,
+    Qt
 )
 from QtFrameless.titleBar import TitleBar
 from QtFrameless.style import stylesheet
-from QtFrameless.cursor import Cursor
 
 
 class FramelessWindow(QMainWindow):
@@ -25,8 +22,6 @@ class FramelessWindow(QMainWindow):
         self.setObjectName("MainWindow")
         self.setContentsMargins(4, 4, 4, 4)
         self._central = QWidget(parent=self)
-        self._central.setMouseTracking(True)
-        self._central.mouseMoveEvent = self.mouseMoveEvent
         self._layout = QVBoxLayout(self._central)
         self._layout.setContentsMargins(0, 0, 0, 0)
         if titleBarClass is not None:
@@ -37,12 +32,7 @@ class FramelessWindow(QMainWindow):
         self._layout.addStretch(1)
         self._main = QWidget(self)
         self._layout.addWidget(self._main)
-        self._direction = None
-        self._cgeom = None
-        self._cpos = None
-        self._pressed = None
         self.statusbar = self.statusBar()
-        self.installEventFilter(self)
         super().setCentralWidget(self._central)
 
     def setTitleBar(self, titleBarClass):
@@ -76,40 +66,6 @@ class FramelessWindow(QMainWindow):
         widget.setContentsMargins(2, 2, 2, 2)
         widget.mouseMoveEvent = self.mouseMoveEvent
         self._layout.addWidget(widget)
-
-    def mousePressEvent(self, event):
-        pos = event.position().toPoint()
-        rect = self.rect()
-        self._direction = Cursor.match(pos, rect, [0, 4, 1])
-        if self.cursor().shape() != self._direction["shape"]:
-            self.setCursor(self._direction["shape"])
-        self._cgeom = self.geometry()
-        self._cpos = pos
-        self._pressed = True
-
-    def mouseReleaseEvent(self, _):
-        self._pressed = False
-        self._cpos = None
-        self._direction = None
-        self._cgeom = None
-
-    def mouseMoveEvent(self, event):
-        geom = self.geometry()
-        pos = event.position().toPoint()
-        if self._pressed and self._direction["id"] != "standard":
-            r = Cursor.resize(self._cpos, pos, geom, self._cgeom, self._direction)
-            min_width, min_height = self.minimumSizeHint().toTuple()
-            if r[2] > min_width and r[3] > min_height:
-                return self.setGeometry(*r)
-        shape = Cursor.match(pos, geom, [0, 1, 4])["shape"]
-        self.setCursor(shape)
-
-    def eventFilter(self, obj, event):
-        if isinstance(event, QMouseEvent):
-            if event.type() == QEvent.MouseMove:
-                print(event)
-        return True
-
 
 
 def execute():
